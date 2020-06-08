4bdef9e3cbb7a7b5e78f4917bcad5bf3d7b137db
Andrew Shankie <andrew@properdesign.co.uk>
2020-06-08 12:31:15 +0100

    Adds react-modal

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { Fragment, useState, useEffect } from "react";
+import Modal from "react-modal";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  const [modalIsOpen, setModalIsOpen] = useState(true);
+
+  Modal.setAppElement("#__next");
+
+  return (
+    <Fragment>
+      <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
+        Oh, hi there!
+      </Modal>
+      <Component {...pageProps} />;
+    </Fragment>
+  );
 };
 
 export default App;
